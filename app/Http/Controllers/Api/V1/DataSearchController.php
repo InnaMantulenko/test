7d2d74dde1e0e902de438dd9ee5d2ca7b4ea6a88
ines <iekostyakova@gmail.com>
2023-08-22 20:45:03 +0300

    Data search API endpoint

---
+++ app/Http/Controllers/Api/V1/DataSearchController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DataResource;
+use App\Services\DataService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class DataSearchController extends Controller
+{
+    /**
+     * DataSearchController constructor.
+     */
+    public function __construct(private DataService $dataService)
+    {
+    }
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $data = DataResource::collection(
+            $this->dataService->search($request->all())
+        );
+
+        return response()->json([
+            'success' => true,
+            'result' => $data
+        ], Response::HTTP_OK);
+    }
+}
---
+++ app/Http/Resources/DataResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DataResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'price' => $this->price,
+            'bedrooms' => $this->bedrooms,
+            'bathrooms' => $this->bathrooms,
+            'storeys' => $this->storeys,
+            'garages' =>$this->garages,
+        ];
+    }
+}
